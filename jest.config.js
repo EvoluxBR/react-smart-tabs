5f1ab1afba2a6dca248733b1945d9de52eb8e196
David Costa <davidcostadev@gmail.com>
2019-08-30 08:59:37 -0300

    Add Lint configurations

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,11 @@
 const { defaults: tsjPreset } = require('ts-jest/presets');
 module.exports = {
   ...tsjPreset,
-  "setupFiles": [
-    "<rootDir>/test-shim.js",
-    "<rootDir>/test-setup.js"
-  ],
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js"
-  ],
-  "collectCoverageFrom": [
-    "src/components/tabs/**/*.{ts,tsx}"
-  ],
-  "transform": {
+  setupFiles: ['<rootDir>/test-shim.js', '<rootDir>/test-setup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  collectCoverageFrom: ['src/components/tabs/**/*.{ts,tsx}'],
+  transform: {
     ...tsjPreset.transform,
-    "^.+\\.svg$": "<rootDir>/svgTransform.js"
-  }
+    '^.+\\.svg$': '<rootDir>/svgTransform.js',
+  },
 };
